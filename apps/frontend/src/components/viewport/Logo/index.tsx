1a7c0b07a7c8e365d777ebac9e2b4323add48a85
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-23 17:07:59 +0100

    Remove header link

--- apps/frontend/src/components/viewport/Logo/index.tsx
+++ apps/frontend/src/components/viewport/Logo/index.tsx
@@ -4,7 +4,6 @@
 
 import { ThemeSwitch } from '@/components/responsive/ThemeSwitch';
 import { Image } from '@mantine/core';
-import Link from 'next/link';
 
 import styles from './styles.module.css';
 
@@ -12,11 +11,11 @@ import styles from './styles.module.css';
 
 export function Logo() {
 	return (
-		<Link className={styles.container} href="/">
+		<div className={styles.container}>
 			<ThemeSwitch
 				dark={<Image alt="Carris Metropolitana" src="/assets/header/static/cmet-header-dark.svg" style={{ height: 70, width: 150 }} />}
 				light={<Image alt="Carris Metropolitana" src="/assets/header/static/cmet-header-light.svg" style={{ height: 70, width: 150 }} />}
 			/>
-		</Link>
+		</div>
 	);
 }
